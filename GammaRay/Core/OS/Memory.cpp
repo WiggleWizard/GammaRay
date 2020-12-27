ca382ebe52a262e0274d9cd17dc37a6fa939ff57
WiggleWizard <1405402+WiggleWizard@users.noreply.github.com>
2020-12-27 16:22:58 +0000

    Absolute memory usage tracking

--- GammaRay/Core/OS/Memory.cpp
+++ GammaRay/Core/OS/Memory.cpp
@@ -4,7 +4,10 @@
 #include "Tracy.hpp"
 
 
-void* operator new(size_t sz)
+uint64_t Memory::memUsage = 0;
+uint64_t Memory::maxUsage = 0;
+
+void* Memory::allocStatic(size_t sz)
 {
     auto ptr = malloc(sz);
 
@@ -12,10 +15,13 @@ void* operator new(size_t sz)
     TracyAlloc(ptr, sz);
 #endif
 
+    // TODO: Thread safety
+    Memory::memUsage += sz;
+
     return ptr;
 }
 
-void* operator new(size_t sz, const char* className)
+void* Memory::allocStatic(size_t sz, const char* className)
 {
     auto ptr = malloc(sz);
 
@@ -23,14 +29,38 @@ void* operator new(size_t sz, const char* className)
     TracyAllocN(ptr, sz, className);
 #endif
 
+    // TODO: Thread safety
+    Memory::memUsage += sz;
+
     return ptr;
 }
 
-void operator delete(void* ptr) noexcept
+void Memory::freeStatic(void* ptr)
 {
+    uint8_t* mem = (uint8_t*)ptr;
+
 #ifdef GR_PROFILE_MEMORY
     TracyFree(ptr);
 #endif
 
+    // TODO: Thread safety
+    uint64_t* s = (uint64_t*)mem;
+    Memory::memUsage -= *s;
+
     free(ptr);
+}
+
+void* operator new(size_t sz)
+{
+    return Memory::allocStatic(sz);
+}
+
+void* operator new(size_t sz, const char* className)
+{
+    return Memory::allocStatic(sz, className);
+}
+
+void operator delete(void* ptr) noexcept
+{
+    Memory::freeStatic(ptr);
 }
--- GammaRay/Core/OS/Memory.h
+++ GammaRay/Core/OS/Memory.h
@@ -1,6 +1,17 @@
 #pragma once
 
 
+class Memory
+{
+    static uint64_t memUsage;
+    static uint64_t maxUsage;
+
+public:
+    static void* allocStatic(size_t sz);
+    static void* allocStatic(size_t sz, const char* className);
+    static void freeStatic(void* ptr);
+};
+
 #define MemNew(m_class) new ( #m_class ) m_class
 
 void* operator new(size_t sz);
