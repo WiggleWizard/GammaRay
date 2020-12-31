4e307bb13ea9076ff80014aeb9647d711fd22b1e
WiggleWizard <1405402+WiggleWizard@users.noreply.github.com>
2020-12-31 13:11:45 +0000

    Moved some shader type stuff to its own header file to avoid cyclic dependencies later

--- GammaRay/Core/Renderer/Buffer.h
+++ GammaRay/Core/Renderer/Buffer.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "RendererShader.h"
+#include "ShaderVariable.h"
 
 
 struct BufferElement
---
+++ GammaRay/Core/Renderer/ShaderVariable.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+enum class ShaderDataType
+{
+    None = 0,
+    Float, Float2, Float3, Float4,
+    Mat3, Mat4,
+    Int, Int2, Int3, Int4,
+    Bool
+};
+
+static uint32_t ShaderDataTypeSize(ShaderDataType type)
+{
+    switch(type)
+    {
+        case ShaderDataType::Float:  return 4;
+        case ShaderDataType::Float2: return 4 * 2;
+        case ShaderDataType::Float3: return 4 * 3;
+        case ShaderDataType::Float4: return 4 * 4;
+        case ShaderDataType::Mat3:   return 4 * 3 * 3;
+        case ShaderDataType::Mat4:   return 4 * 4 * 4;
+        case ShaderDataType::Int:    return 4;
+        case ShaderDataType::Int2:   return 4 * 2;
+        case ShaderDataType::Int3:   return 4 * 3;
+        case ShaderDataType::Int4:   return 4 * 4;
+        case ShaderDataType::Bool:   return 1;
+    }
+
+    GR_CORE_ASSERT(false, "Shader data size could not be determined from type");
+    return 0;
+}
